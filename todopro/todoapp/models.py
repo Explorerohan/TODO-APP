7003b5374e92372b2088ca4ac6e2ea8c357c2cdf
Rohan Rai <Rohan0004444@gmail.com>
2024-09-11 21:52:12 +0545

    Improved the frontend and made user friendly

--- todopro/todoapp/models.py
+++ todopro/todoapp/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Todo(models.Model):
     task = models.CharField( max_length=50)
     date = models.DateTimeField()
+    time = models.TimeField(default=timezone.now)
     priorities = models.CharField(max_length=50)
--- todopro/todoapp/views.py
+++ todopro/todoapp/views.py
@@ -1,21 +1,48 @@
 from django.shortcuts import render,redirect
 from .models import Todo
 from django.utils.timezone import now
-from datetime import timedelta
+from datetime import timedelta,datetime
 # Create your views here.
+from datetime import datetime, timedelta
+
 def add_todo(request):
     if request.method == 'POST':
         data = request.POST
         task = data.get('add-todo')
         date = data.get('due-date')
+        time = data.get('due-time') 
         priorities = data.get('priority')
-        Todo.objects.create(task=task, date = date, priorities=priorities)
+        
+        Todo.objects.create(task=task, date=date,time=time, priorities=priorities)
         return redirect('/')
+
     queryset = Todo.objects.all()
     context = {'tasks': queryset}
+
+    # Calculate the exact time left for each task
     for task in queryset:
-        task.time_left = calculate_time_left(task.date)
-    return render(request, 'index.html',context)
+        if task.date and task.time:
+            due_datetime = datetime.combine(task.date, task.time)
+            now = datetime.now()
+            time_left = due_datetime - now
+            
+            if time_left < timedelta():
+                task.time_left = "Overdue"
+            else:
+                days_left = time_left.days
+                hours_left, remainder = divmod(time_left.seconds, 3600)
+                minutes_left, seconds_left = divmod(remainder, 60)
+                if days_left:
+                    task.time_left = f"{days_left} days {hours_left} hours {minutes_left} minutes"
+                elif hours_left:
+                    task.time_left = f"{hours_left} hours {minutes_left} minutes"
+                else:
+                    task.time_left = f"{minutes_left} minutes"
+        else:
+            task.time_left = "Incomplete date/time information"
+
+    return render(request, 'index.html', context)
+
 
 def calculate_time_left(due_date):
     if due_date is None:
